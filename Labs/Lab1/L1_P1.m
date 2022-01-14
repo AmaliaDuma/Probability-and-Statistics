f46cfbebd6dfae98ef3a0a6dbeb1749e860d0c30
AmaliaDuma <72137434+AmaliaDuma@users.noreply.github.com>
2022-01-15 01:33:36 +0200

    Basic operations, matrix

---
+++ Labs/Lab1/L1_P1.m
@@ -0,0 +1,10 @@
+% write a matrix: every row follwed by ;
+A = [1 0 -2; 2 1 3; 0 1 0];
+B = [2 1 1; 1 0 -1; 1 1 0];
+
+% operations:
+C = A-B;
+% standard matrix multiplication: *
+D = A*B;
+%element-wise multiplication: .*; . can be applied to / and ^
+E = A.*B
